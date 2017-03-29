5eff440ae2c41528abf6c0ac4b2ff68bf1920a51
kaya winn and Riley Watts <riley@>
2017-03-29 15:25:07 -0700

    add index template content

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,6 +11,7 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("regions", Region.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
